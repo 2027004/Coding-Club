23fe7c590a4362023170817e034cb6b7b05dda6f
2028061-Luke <2028061@sluh.org>
2025-04-14 10:42:59 -0500

    Update example.py

--- example.py
+++ example.py
@@ -8,3 +8,59 @@ print("Hello world")
 print(f"I am so glad you are feeling {day}")
 
 print("Good job in the musical Luke!")
+
+import random
+yourList = ["rock", "paper", "scissors"]
+play = ("y")
+cluck = 0
+while play == ("y"):
+    hand = input("choose one, rock, paper, or scissors ")
+    random.shuffle(yourList)
+
+    while hand == ("scissors"):
+
+        if yourList[1] == "rock":
+            print("lose")
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+        if yourList[1] == "paper":
+            print("win")
+            cluck = cluck + 1
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+        if yourList[1] == "scissors":
+            print("tie")
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+
+    while hand == "rock":
+        if yourList[1] == "rock":
+            print("tie")
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+        if yourList[1] == "paper":
+            print("lose")
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+        if yourList[1] == "scissors":
+            print("win")
+            cluck = cluck + 1
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+    while hand == ("paper"):
+        
+        if yourList[1] == "rock":
+            print("win")
+            cluck = cluck + 1
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+        if yourList[1] == "paper":
+            print("tie")
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+        if yourList[1] == "scissors":
+            print("lose")
+            play = input("do you want to play again y/n ")
+            hand = ("choose one, rock, paper, or scissors ")
+
+print(f"your score was {cluck}")
