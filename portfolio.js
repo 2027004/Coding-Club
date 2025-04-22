34edaaffdf91f9e47e4dc66e3cdce4846dd00fd9
2027004 <2027004@sluh.org>
2025-04-22 16:10:30 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -10,10 +10,12 @@ bgColor = document.getElementById("bgColor");
 // pop-up menus
 write = document.getElementById("write");
     write.style.display="none";
-colors = document.getElementById("colors")
+colors = document.getElementById("colors");
     colors.style.display="none";
-margin = document.getElementById("margin")
+margin = document.getElementById("margin");
     margin.style.display="none";
+templates = document.getElementById("templates");
+    templates.style.display="none";
     
 
 
@@ -66,6 +68,8 @@ function marginPop(){
 }
 
 
+
+
 function nameCloser(){
     write.style.display= "none";
 }
@@ -76,70 +80,8 @@ function marginCloser(){
     margin.style.display = "none";
 }
 
-
 //Make the DIV element draggagle:
-dragElement(write);
-dragElement(colors);
-dragElement(margin);
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id+"headbar")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id+"headbar").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-
-/* MOVES TO FRONT */
-write.addEventListener('mousedown', (ev) => {
-    write.style.zIndex = 3;
-    colors.style.zIndex = 2;
-    margin.style.zIndex = 2;
-})
-colors.addEventListener('mousedown', (ev) => {
-    colors.style.zIndex = 3;
-    write.style.zIndex = 2;
-    margin.style.zIndex = 2;
-})
-margin.addEventListener('mousedown', (ev) => {
-    colors.style.zIndex = 2;
-    write.style.zIndex = 2;
-    margin.style.zIndex = 3;
-})
 
 /* CHANGE COLOR BUTTON */
 function changeColor(color){
@@ -247,6 +189,8 @@ function moveBiog(direction){
         biog.style.top = currentDirect + "px";
     }
 }
+
+
 
 window.onload = async () => {
     const res = await fetch("/my-page");
