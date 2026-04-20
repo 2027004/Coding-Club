154b3a56eda4c0a757e1e3b5d20710fb2f653c49
2028364 <2028364@sluh.org>
2026-04-20 10:57:38 -0500

    Update CalculatorFunction.js
    Final

--- HTML/CalculatorFunction.js
+++ HTML/CalculatorFunction.js
@@ -1,4 +1,33 @@
-function appendToDisplay()
-{
 
+const display = document.getElementById("display")
+const tester = ["", "", ""];
+
+
+
+
+function appendToDisplay(input)
+{
+    if(display.value =="Cannot Compute.")
+    {
+        display.value = null;
+    }
+    display.value += input;
+}
+
+function clearDisplay()
+{
+    display.value = null;
+}
+
+function calculate()
+{
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error)
+    {
+        display.value = "Cannot Compute.";
+    }
+    
+    
 }
